26b504c248079ab5f0d92c290ac18a7c8d6a87c3
dev-jdss <2023_jose.salinas@iticbcn.cat>
2023-11-04 21:58:46 +0100

    Controlador API senzillo(errores en PUT, DELETE)

--- Proyecto1DAM_DAW/src/main/java/com/example/demo/controller/EmpresaControlador.java
+++ Proyecto1DAM_DAW/src/main/java/com/example/demo/controller/EmpresaControlador.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,20 +18,73 @@ import com.example.demo.model.Empresa;
 import com.example.demo.repository.EmpresaRepository;
 
 @RestController
-@RequestMapping("/empresas")
+@RequestMapping("/")
 public class EmpresaControlador {
 	@Autowired
-	private final EmpresaRepository empresaRepo;
+	private final EmpresaRepository repository;
 	
-	public EmpresaControlador (EmpresaRepository empresaRepo) {
-		this.empresaRepo = empresaRepo;
+	public EmpresaControlador (EmpresaRepository repository) {
+		this.repository = repository;
 		
 	}
 	
-	@GetMapping
-	public ResponseEntity<List<Empresa>> getEmpresas() {
-		List<Empresa> empresas = empresaRepo.findAll();
+	@GetMapping("/empresas")
+	public ResponseEntity<List<Empresa>> readEmpresas() {
+		List<Empresa> empresas = repository.findAll();
 		return ResponseEntity.ok(empresas);
+	}
+	
+	
+	
+	
+	@GetMapping("/empresas/{id}")
+	public ResponseEntity<Empresa> readEmpresa(@PathVariable int id) {
+		Optional<Empresa> empresaBuscada = repository.findById(id);
+		
+		if (empresaBuscada.isPresent())
+			return ResponseEntity.ok(empresaBuscada.get());
+		else
+			return ResponseEntity.noContent().build();
+	}
+	
+	
+	
+	
+	@PostMapping("/empresas")
+	public ResponseEntity<Empresa> createEmpresa(@RequestBody Empresa empresa) {
+		Empresa empresaNueva = repository.save(empresa);
+		return ResponseEntity.ok(empresaNueva);
+	}
+	
+	
+	
+	
+	@PutMapping("empresas/{id}")
+	public ResponseEntity<Empresa> updateEmpresa(@PathVariable int id, Empresa empresa) {
+		Optional<Empresa> empresaBuscada = repository.findById(id);
+		if (empresaBuscada.isPresent()) {
+			Empresa empresaCambiada = empresaBuscada.get();
+			
+			empresaCambiada.setNombre(empresa.getNombre());
+			empresaCambiada.setSector(empresa.getSector());
+			empresaCambiada.setTamaño(empresa.getTamaño());
+			empresaCambiada.setTipo(empresa.getTipo());
+			empresaCambiada.setUbicacion(empresa.getUbicacion());
+			repository.save(empresaCambiada);
+			return ResponseEntity.ok(empresaCambiada);
+		}
+		else {
+			return ResponseEntity.noContent().build();
+		}
+	}
+	
+	
+	
+	
+	@DeleteMapping("/empresas/{id}")
+	public ResponseEntity<Void> deleteEmpresa(@PathVariable int id) {
+		repository.deleteById(id);
+		return ResponseEntity.ok(null);
 	}
 	
 }
