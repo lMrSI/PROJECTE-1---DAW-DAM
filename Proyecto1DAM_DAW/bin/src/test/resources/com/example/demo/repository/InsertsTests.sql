6b1c7694f39020f0200ebc8c8257e975a24b75be
lMrSl <apalenqu22@jaumebalmes.net>
2024-01-10 16:47:00 +0100

    seguridad

---
+++ Proyecto1DAM_DAW/bin/src/test/resources/com/example/demo/repository/InsertsTests.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+    empresas (nombre, sector, tamaño, tipo, ubicacion)
+VALUES
+    ('Facebook', 'Telecomunicaciones', 'Mediana', 'S.S.A', 'USA'),
+    ('Google', 'Consultoria', 'Grande', 'S.A', 'USA');
