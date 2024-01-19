596ab4d9d71b38544267c5dbd7bdc5c1a0e61634
Jose Daniel <146064921+dam-JoseSalinas@users.noreply.github.com>
2024-01-19 15:36:21 +0100

    Delete BolsaDeTrabajo directory

--- BolsaDeTrabajo/BolsaDeTrabajo/src/main/java/com/example/demo/dao/EmpresaDAO.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.Empresa;
-
-public interface EmpresaDAO extends JpaRepository<Empresa, Long>{
-	
-}
