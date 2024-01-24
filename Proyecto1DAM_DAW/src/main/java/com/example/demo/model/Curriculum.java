9863d77c263edcf11ed833807a406738defc5e51
JoseSalinas <2023_jose.salinas@iticbcn.cat>
2024-01-24 01:54:45 +0100

    Alumno-Oferta y lista de Ofertas

--- Proyecto1DAM_DAW/src/main/java/com/example/demo/model/Curriculum.java
+++ Proyecto1DAM_DAW/src/main/java/com/example/demo/model/Curriculum.java
@@ -1,12 +1,10 @@
 package com.example.demo.model;
 
 import java.util.Objects;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import jakarta.persistence.Table;
 
 @Entity
--- Proyecto1DAM_DAW/src/main/java/com/example/demo/repository/OfertaRepository.java
+++ Proyecto1DAM_DAW/src/main/java/com/example/demo/repository/OfertaRepository.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.example.demo.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Empresa;
@@ -15,6 +16,7 @@ public interface OfertaRepository extends JpaRepository<Oferta, Integer> {
 	List<Oferta> findByEmpresa(Empresa empresa); //Busca Ofertas por Empresa
 	List<Oferta> findByEmpresaIsNull();
 
+	List<Oferta> findOfertasByUsuarios(Usuario usuario);
 	//Tests
 	//List<Oferta> findAll();
 	//Optional<Oferta> findById();
